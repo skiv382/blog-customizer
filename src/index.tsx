a550e1ef7a12506cf536fbadab794391635e5e1e
Pashkov Artem <skiv382@gmail.com>
2025-10-16 19:49:50 +0300

    feat: implement sidebar toggle, form with Select/RadioGroup, apply/reset logic, and app-level CSS variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, type ArticleStateType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,22 +13,34 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
-			<Article />
-		</main>
-	);
+    const [appliedArticleState, setAppliedArticleState] = useState<ArticleStateType>(
+        defaultArticleState
+    );
+
+    const handleApply = (nextState: ArticleStateType) => {
+        setAppliedArticleState(nextState);
+    };
+
+    return (
+        <main
+            className={clsx(styles.main)}
+            style={
+                {
+                    '--font-family': appliedArticleState.fontFamilyOption.value,
+                    '--font-size': appliedArticleState.fontSizeOption.value,
+                    '--font-color': appliedArticleState.fontColor.value,
+                    '--container-width': appliedArticleState.contentWidth.value,
+                    '--bg-color': appliedArticleState.backgroundColor.value,
+                } as CSSProperties
+            }>
+            <ArticleParamsForm
+                initialState={appliedArticleState}
+                defaults={defaultArticleState}
+                onApply={handleApply}
+            />
+            <Article />
+        </main>
+    );
 };
 
 root.render(
